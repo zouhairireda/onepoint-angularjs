de3f8eb9faaae2da6a66093ffa56b09525ace9bb
David MAURIN <d.maurin@groupeonepoint.com>
2016-11-27 14:09:28 +0100

    Remove slider component

--- src/app/common/directives/magnific-popup.module.js
+++
@@ -1,7 +0,0 @@
-import 'jquery';
-import 'magnific-popup';
-import angular from 'angular';
-import magnificPopup from './magnific-popup.directive';
-
-angular.module('shopping.common.magnificPopup', [])
-  .directive('magnificPopup', magnificPopup);
