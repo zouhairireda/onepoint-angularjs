1373a508d1d4ae1bcdb50c2908ce4ff14accaa5f
David MAURIN <d.maurin@groupeonepoint.com>
2016-11-28 17:18:42 +0100

    Activate Webpack HtmlPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,51 +4,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval-source-map',
-    // devtool: 'eval-source-map',
-    // devtool: 'inline-sourcemap',
-    // devtool: 'cheap-module-source-map',
-    entry:[
-        // 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true',
-        './app/main.js'
-    ],
+  devtool: 'eval-source-map',
+  entry: [
+    './app/main.js'
+  ],
 
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'app.js'
-    },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'app.js'
+  },
 
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        // new HtmlWebpackPlugin({
-        //     template: 'app/index.tpl.html',
-        //     inject: 'body',
-        //     filename: 'index.html'
-        // }),
-        new webpack.ProvidePlugin({
-            jQuery: 'jquery',
-            $: 'jquery',
-            jquery: 'jquery',
-            'window.jQuery': 'jquery',
-            'window.$': 'jquery'
-        }),
-        new CopyWebpackPlugin([{ from: 'assets/img', to: 'img' }], {})
-        // new webpack.DefinePlugin({
-        //     'process.env.NODE_ENV': JSON.stringify('development')
-        // })
-    ],
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery',
+      jquery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery'
+    }),
+    new CopyWebpackPlugin([{from: 'assets/img', to: 'img'}], {})
+  ],
 
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015'] } },
-            { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
-            { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
-            { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' },
-            { test: /\.html/, exclude: /node_modules/, loader: 'html-loader' },
-            { test: /\.(png|jpg|jpeg|gif)$/, loader: 'file-loader?name=assets/img/img-[hash:6].[ext]' }
-            //   {test: /\.s?css$/, loader: 'style!css!sass?includePaths[]=' + bourbon },
-        ]
-    }
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets: ['es2015']}},
+      {test: /\.scss$/, loaders: ['style', 'css', 'sass']},
+      {test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000'},
+      {test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery'},
+      {test: /\.html/, exclude: /node_modules/, loader: 'html-loader'},
+      {test: /\.(png|jpg|jpeg|gif)$/, loader: 'file-loader?name=assets/img/img-[hash:6].[ext]'}
+    ]
+  }
 };
