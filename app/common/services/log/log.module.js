642f2e29bd105d0e3a10634e8d8abf4cee0e7d90
David MAURIN <d.maurin@groupeonepoint.com>
2016-11-28 18:07:34 +0100

    Remove Log decorator

--- app/common/services/log/log.module.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-import 'angular-resource';
-import {configureLogDecorator} from './log.decorator';
-
-angular.module('shopping.services.log', [])
-  .config(configureLogDecorator);
--- app/main.js
+++ app/main.js
@@ -5,7 +5,6 @@ import angular from 'angular';
 import './layout/layout.module.js';
 import './features/product/product.module.js';
 import './features/account/account.module';
-import './common/services/log/log.module';
 import './common/services/router/router.module';
 import './common/services/bootstrap/bootstrap.module';
 
@@ -14,13 +13,9 @@ angular
     'shopping.layout',
     'shopping.feature.product',
     'shopping.feature.account',
-    'shopping.services.log',
     'shopping.services.router',
     'shopping.services.bootstrap'
   ])
-  .run(($log) => {
-    $log.debug('Test log debug decorator', ['param1', {name: 'params2'}]);
-  })
   .run((shopBootstrapService) => shopBootstrapService.process());
 
 // Authorise WebPack Hot module replacement
